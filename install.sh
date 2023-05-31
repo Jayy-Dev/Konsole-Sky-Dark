488a0d7bfd88549ceb579276eeb8059a4252ed98
JayyDev <105900291+Jayy-Dev@users.noreply.github.com>
2023-05-31 16:28:47 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
-# Get the directory path of the script
-SRC_DIR="$(cd "$(dirname "$0")" && pwd)"
+# Confirmation prompt
+read -p "Do you want to install this theme? (y/n): " confirm
 
-# Specify the file to be installed
-file_path="$SRC_DIR/Catppuccin Sky-Dark.colorscheme"
+# Check user's confirmation
+if [[ $confirm =~ ^[Yy]$ ]]; then
+    # Get the directory path of the script
+    SRC_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-# Specify the destination directory
-destination_dir="$HOME/.local/share/konsole/"
+    # Specify the file to be installed
+    file_path="$SRC_DIR/Catppuccin Sky-Dark.colorscheme"
 
-# Check if the destination directory exists
-if [ ! -d "$destination_dir" ]; then
-    echo "Destination directory does not exist. Creating..."
-    mkdir -p "$destination_dir"
-fi
+    # Specify the destination directory
+    destination_dir="$HOME/.local/share/konsole/"
 
-# Copy the file to the destination directory
-cp "$file_path" "$destination_dir"
+    # Check if the destination directory exists
+    if [ ! -d "$destination_dir" ]; then
+        echo "Destination directory does not exist. Creating..."
+        mkdir -p "$destination_dir"
+    fi
 
-# Check if the copy was successful
-if [ $? -eq 0 ]; then
-    echo "File installed successfully!"
+    # Copy the file to the destination directory
+    cp "$file_path" "$destination_dir"
+
+    # Check if the copy was successful
+    if [ $? -eq 0 ]; then
+        echo "File installed successfully!"
+    else
+        echo "Error occurred while installing the file."
+    fi
 else
-    echo "Error occurred while installing the file."
+    echo "Installation cancelled."
 fi
